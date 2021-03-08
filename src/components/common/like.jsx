88c22abdca34241d5ce75b7f3f7b2481471b32c0
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-03-08 16:54:36 -0500

    Added a sorting option of ascending and descending for the titles, genre, stock and rate.

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,5 +1,3 @@
-import React, { Component } from 'react';
-
 //Input : liked: boolean
 //Output : onClick 
 
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,17 +5,19 @@ import Pagination from "./common/pagination";
 import {paginate} from "../utils/paginate";
 import ListGroup from "./common/listGroup";
 import MoviesTable from './moviesTable';
+import _ from "lodash";
 
 class Movies extends Component {
     state = { 
         movies: [],
         genres: [],
         currentPage: 1,
-        pageSize: 4
+        pageSize: 4,
+        sortColumn: {path: 'title', order: 'asc'}
     }
 
     componentDidMount() {
-        const genres = [{ name: "All Genres" }, ...getGenres()]
+        const genres = [{_id:"", name: "All Genres" }, ...getGenres()]
 
         this.setState({ movies: getMovies(), genres });
     }
@@ -41,9 +43,28 @@ class Movies extends Component {
         this.setState({ selectedGenre: genre, currentPage: 1 });
     }
 
+    handleSort = path => {
+        const sortColumn = {...this.state.sortColumn};
+
+        if (sortColumn.path === path)
+            sortColumn.order = (sortColumn.order === 'asc') ? 'desc': 'asc';
+        else {
+            sortColumn.path = path;
+            sortColumn.order = 'asc';
+        }
+
+        this.setState({ sortColumn });
+    }
+
     render() { 
         const { length: count } = this.state.movies;
-        const {pageSize, currentPage, selectedGenre, movies: allMovies} = this.state;
+        const {
+            pageSize, 
+            currentPage, 
+            sortColumn,
+            selectedGenre, 
+            movies: allMovies
+        } = this.state;
 
         if(count === 0 ) return <p>There are no movies in the database!</p>;
 
@@ -51,7 +72,9 @@ class Movies extends Component {
         ? allMovies.filter(m => m.genre._id === selectedGenre._id) 
         : allMovies;
 
-        const movies = paginate(filtered, currentPage, pageSize);
+        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+        const movies = paginate(sorted, currentPage, pageSize);
 
         return (
             <div className="row">
@@ -71,6 +94,7 @@ class Movies extends Component {
                     movies={movies}
                     onLike={this.handleLike}
                     onDelete={this.handleDelete}
+                    onSort={this.handleSort}
                     />
 
                     <Pagination 
