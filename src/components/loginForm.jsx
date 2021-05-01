b0b4fc7b84300b8d6b31f6db60e14885fe7d9582
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-05-01 18:51:20 -0400

    Added controlled elements to the loginForm and handling username and password.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+	state = {
+		account: { username: "", password: "" },
+	};
+
 	username = React.createRef();
 
 	// componentDidMount() {
@@ -22,7 +26,14 @@ class LoginForm extends Component {
 		console.log("");
 	};
 
+	handleChange = ({ currentTarget: input }) => {
+		const account = { ...this.state.account };
+		account[input.name] = input.value;
+		this.setState({ account });
+	};
+
 	render() {
+		const { account } = this.state;
 		return (
 			<div>
 				<h1>Login</h1>
@@ -30,16 +41,26 @@ class LoginForm extends Component {
 					<div className="form-group">
 						<label htmlFor="username">Username</label>
 						<input
+							value={account.username}
+							onChange={this.handleChange}
 							autoFocus
 							ref={this.username}
 							id="username"
+							name="username"
 							type="text"
 							className="form-control"
 						/>
 					</div>
 					<div className="form-group">
 						<label htmlFor="password">Password</label>
-						<input id="password" type="text" className="form-control" />
+						<input
+							value={account.password}
+							onChange={this.handleChange}
+							name="password"
+							id="password"
+							type="text"
+							className="form-control"
+						/>
 					</div>
 					<button className="btn btn-primary">Login</button>
 				</form>
